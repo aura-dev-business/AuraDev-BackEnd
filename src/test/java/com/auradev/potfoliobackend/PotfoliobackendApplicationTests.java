eee40ede81ad06728e7f97da056c3819a6433497
jouhar <jouharalikv@gmail.com>
2025-03-29 02:53:59 +0530

    Initial Commit

---
+++ src/test/java/com/auradev/potfoliobackend/PotfoliobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auradev.potfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotfoliobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
