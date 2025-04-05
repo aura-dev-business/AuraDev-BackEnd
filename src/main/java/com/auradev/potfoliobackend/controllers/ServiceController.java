6cd035501aed7fb01251631c963a839e90375cbb
jouhar <jouharalikv@gmail.com>
2025-04-05 16:21:50 +0530

    Service Entity

---
+++ src/main/java/com/auradev/potfoliobackend/controllers/ServiceController.java
@@ -0,0 +1,28 @@
+package com.auradev.potfoliobackend.controllers;
+
+import com.auradev.potfoliobackend.models.ServiceEntity;
+import com.auradev.potfoliobackend.services.ServiceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/services")
+public class ServiceController {
+
+    private final ServiceService serviceService;
+
+    public ServiceController(ServiceService serviceService) {
+        this.serviceService = serviceService;
+    }
+
+    @GetMapping
+    public List<ServiceEntity> getAllServices() {
+        return serviceService.getAllServices();
+    }
+
+    @PostMapping
+    public ServiceEntity createService(@RequestBody ServiceEntity service) {
+        return serviceService.createService(service);
+    }
+}
