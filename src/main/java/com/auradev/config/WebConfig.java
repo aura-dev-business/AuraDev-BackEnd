ede6972206ec823027a7ece2fd4c6014d03bc381
aura-dev-business <auradevbusiness@gmail.com>
2025-04-14 09:24:17 +0530

    Update WebConfig.java

--- src/main/java/com/auradev/config/WebConfig.java
+++ src/main/java/com/auradev/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("https://www.auradev.in")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
